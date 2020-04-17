0d35194f66d69c9e9fb8ce0ad22392689b931bdc
Darksiend <logosmk2@gmail.com>
2020-04-17 19:00:18 +0300

    Add Error Boundry

---
+++ src/components/error-boundry/error-boundry.js
@@ -0,0 +1,24 @@
+export React, {Component} from 'react';
+
+import '../error-indicator/error-indicator';
+
+export default class ErrorBoundry extends Component {
+
+    state = {
+        hasError: false
+
+    };
+
+    componentDidCatch() {
+        this.setState({hasError: true});
+
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <ErrorIndicator/>
+        }
+        return this.props.children;
+    }
+
+}
