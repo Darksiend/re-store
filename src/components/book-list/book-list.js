8879736cbbbd278f0a1b4a875a924e97ba3ebf0d
Darksiend <logosmk2@gmail.com>
2020-04-21 22:31:44 +0300

    Refactorng names of actions

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import BookListItem from '../book-list-item';
 import './book-list.css';
 import {connect} from 'react-redux';
-import {booksLoaded, booksRequested, booksError} from "../../actions";
+import {fetchBooks} from "../../actions";
 import {withBookstoreService} from '../hoc';
 import {compose} from '../../utils';
 import Spinner from '../spinner';
@@ -11,10 +11,7 @@ import ErrorIndicator from '../error-indicator'
 class BookList extends Component {
 
     componentDidMount() {
-        //1. recive data
-        const {bookstoreService, booksLoaded, booksRequested, booksError} = this.props;
-        booksRequested();
-        bookstoreService.getBooks().then((data) => booksLoaded(data)).catch((err) => booksError(err));
+        this.props.fetchBooks();
     }
 
 
@@ -46,12 +43,12 @@ const mapStateToProps = ({books, loading, error}) => {
     return {books, loading, error};
 };
 
-const mapDispatchToProps = {
-    booksLoaded,
-    booksRequested,
-    booksError
-};
+const mapDispatchToProps = (dispatch, {bookstoreService}) => {
 
+    return {
+        fetchBooks: fetchBooks(bookstoreService, dispatch)
+    };
+};
 
 export default compose(
     withBookstoreService(),
